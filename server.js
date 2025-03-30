0bfcfb3f90d1395805e6e8233c8fca62bc6539ef
Shashikant Prasad <shashikantprasad107@gmail.com>
2025-03-30 06:19:24 +0530

    testing

--- server.js
+++ server.js
@@ -22,20 +22,20 @@ app.listen(healthCheckPort, () => {
   console.log(`Health check server is running on http://localhost:${healthCheckPort}/healthcheck`);
 });
 
-// Create a new WebSocket server
-const wss = new WebSocket.Server({ port: port }, () => {
-  console.log(`WebSocket server is running on ws://localhost:${port}`);
-});
+// // Create a new WebSocket server
+// const wss = new WebSocket.Server({ port: port }, () => {
+//   console.log(`WebSocket server is running on ws://localhost:${port}`);
+// });
 
-// Broadcast incoming messages to all connected clients
-wss.on('connection', function connection(ws) {
-  ws.on('message', function incoming(message) {
-    console.log('received: %s', message);
-    // Broadcast to every connected client
-    wss.clients.forEach(function each(client) {
-      if (client.readyState === WebSocket.OPEN) {
-        client.send(message.toString());
-      }
-    });
-  });
-});
+// // Broadcast incoming messages to all connected clients
+// wss.on('connection', function connection(ws) {
+//   ws.on('message', function incoming(message) {
+//     console.log('received: %s', message);
+//     // Broadcast to every connected client
+//     wss.clients.forEach(function each(client) {
+//       if (client.readyState === WebSocket.OPEN) {
+//         client.send(message.toString());
+//       }
+//     });
+//   });
+// });
