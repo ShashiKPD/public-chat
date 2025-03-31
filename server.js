1daa85a5b4e477e65a11fdc35c109a295e3259ff
Shashikant Prasad <shashikantprasad107@gmail.com>
2025-04-01 01:12:49 +0530

    sdf

--- server.js
+++ server.js
@@ -43,8 +43,8 @@ app.get('/connect', function (req, res) {
   }
   cookieOptions = { 
     httpOnly: true, 
-    // secure: process.env.NODE_ENV === "production", 
-    // sameSite: (process.env.NODE_ENV === "production") ? 'none' : '',
+    secure: process.env.NODE_ENV === "production", 
+    sameSite: (process.env.NODE_ENV === "production") ? 'none' : '',
     maxAge: 24 * 60 * 60 * 1000
   }
 
