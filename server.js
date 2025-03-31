6bb4eefba25cd379109a6700631dbd570a23a65a
Shashikant Prasad <shashikantprasad107@gmail.com>
2025-04-01 02:52:55 +0530

    found the issue: apple's age old third party cookie policy

--- server.js
+++ server.js
@@ -42,10 +42,10 @@ app.get('/connect', function (req, res) {
     userColor: colors[Math.floor(Math.random() * 100)]
   }
   cookieOptions = { 
-    // httpOnly: false, 
+    httpOnly: false, 
     secure: process.env.NODE_ENV === "production", 
     sameSite: (process.env.NODE_ENV === "production") ? 'none' : '',
-    // maxAge: 24 * 60 * 60 * 1000
+    maxAge: 24 * 60 * 60 * 1000
   }
   // if(req.headers['user-agent'] == process.env.THAT_USER_AGENT) {
   //   cookieOptions.httpOnly = false;
