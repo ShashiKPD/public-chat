6bd9f7dc9b3ba97c8bd95efae125cd9886ae9d69
Shashikant Prasad <shashikantprasad107@gmail.com>
2025-03-31 22:07:26 +0530

    removes secure

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ app.get('/connect', function (req, res) {
   }
   cookieOptions = { 
     httpOnly: true, 
-    secure: process.env.NODE_ENV === "production", 
+    // secure: process.env.NODE_ENV === "production", 
     // sameSite: (process.env.NODE_ENV === "production") ? 'none' : '',
     maxAge: 24 * 60 * 60 * 1000
   }
